135a05554f795bd2b7823379bce9734a8367749c
Matheus <matheusfranco876@gmail.com>
2020-08-30 15:28:59 -0400

    adicionando pasta

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+
+
 print("\t"+"\033[33m----\033[m"*9)
 print("\t\033[32m|Ananlizador de arquivos Presenstes|\033[m")
 print("\t"+"\033[33m----\033[m"*9)
+
+pasta_atual = os.path.dirname(os.path.abspath(__file__))
+
+
+
+print()
