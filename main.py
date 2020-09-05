6b6a67d6de2774fee9e63fbdff55428647abcf81
Matheus <matheusfranco876@gmail.com>
2020-09-04 22:10:19 -0400

    configurando pastas

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import os
 
 
-print("\t"+"\033[33m----\033[m"*9)
-print("\t\033[32m|Ananlizador de arquivos Presenstes|\033[m")
-print("\t"+"\033[33m----\033[m"*9)
 
-pasta_atual = os.path.dirname(os.path.abspath(__file__))
+def criar_pasta_local():
+    pasta_atual = os.path.dirname(os.path.abspath(__file__))
+    #lista_aquivos = os.listdir(pasta_atual)
+    for (dirpath, dirnames, filenames) in os.walk(pasta_atual):
+        print(filenames)
+  
 
 
+if __name__ == "__main__":
 
-print()
+    print("\t"+"\033[33m----\033[m"*7)
+    print("\t\033[32m |Organizador De Arquivos|\033[m")
+    print("\t"+"\033[33m----\033[m"*7)
+    print("--> Escolhendo as Opções")
+    escolha = str(input("--> Digite A Sua escolho:> "))
+    if(escolha == '1'):
+        criar_pasta_local()
+    elif(escolha == '2'):
+        pass
+    else:
+        print("\033[31mEscolha Invalida\033[m")
