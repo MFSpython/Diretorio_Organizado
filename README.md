932ce6c4112b244a7872081ca1b6e36d9d0ad95f
Matheus <matheusfranco876@gmail.com>
2020-09-05 14:00:05 -0400

    Programa Em Funcionamento,Mas  Problemas no linux

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# teste
-teste
+# Organizador Diretorio
+    O Intuito desse Diretorio e Criar Script Python
+Para organizar Arquivos Em Diretorios
--- main.py
+++ main.py
@@ -1,37 +1,72 @@
 import os
+import shutil
 
 pasta_atual = os.path.dirname(os.path.abspath(__file__))
 
 
-def cria_pasta_move(lista_aquivos):
-    os.chdir(pasta_atual)
-    pass
+def cria_pasta():
+    lista_arquivos, diretorios = indentificador_diretorio_Atual()
+    for arquivo in lista_arquivos:
+        arq = os.path.splitext(arquivo)[1][1:].capitalize()
+
+        if arquivo.lower() == 'main.py':
+            print(f"Arquivo {arquivo} inguinorado")
+
+        else:
+            if arq not in diretorios and arq != "":
+
+                os.mkdir(pasta_atual + os.sep + arq)
+                print(f"Criando Pasta \033[32m{arq}\033[m")
+
+                shutil.move(arquivo, pasta_atual + os.sep + arq)
+                print(
+                    f"Movendo \033[33m{arquivo}\033[m Para \033[34m{arq}\033[m")
+
+            else:
+                shutil.move(arquivo, pasta_atual + os.sep + arq)
+                print(
+                    f"Movendo \033[33m{arquivo}\033[m Para \033[34m{arq}\033[m")
+
+            '''
+            if arq not in diretorios and arq != "" and arquivo.lower() != "main.py":
+                os.mkdir(pasta_atual+os.sep+arq)
+                print(f"Pasta {arq} foi Criada")
+
+                os.rename(pasta_atual+os.sep+arquivo,
+                        pasta_atual+os.sep+arq+arquivo)
+                print(
+                    f">>\033[34m{arquivo}\033[m Foi Movido Para \033[32m{arq}\033[m")
+            else:
+                os.rename(pasta_atual+os.sep+arquivo,
+                        pasta_atual+os.sep+arq+arquivo)
+            '''
 
 
 def indentificador_diretorio_Atual():
-
+    lista_diretorios = []
     #lista_aquivos = os.listdir(pasta_atual)
     for (dirpath, dirnames, filenames) in os.walk(pasta_atual):
         if(dirpath == pasta_atual):
-            return filenames
-        else:
-            continue
+            return filenames, dirnames
 
 
 def main():
 
+    os.system("clear || cls")
     print("\t"+"\033[33m----\033[m"*7)
     print("\t\033[32m |Organizador De Arquivos|\033[m")
     print("\t"+"\033[33m----\033[m"*7)
     print("--> Escolhendo as Opções")
     escolha = str(input("--> Digite A Sua escolho:> "))
+    #escolha = '1'
     if(escolha == '1'):
-        cria_pasta_move(indentificador_diretorio_Atual())
+        # cria_pasta_move(indentificador_diretorio_Atual())
+        cria_pasta()
     elif(escolha == '2'):
         pass
     else:
         print("\033[31mEscolha Invalida\033[m")
-
+    print("\033[36mFim Do Programa\033[m")
 
 if __name__ == "__main__":
 
