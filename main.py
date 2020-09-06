e3cdc499b14d7cf18797d1a2ce85216a4a4b99b6
Matheus <matheusfranco876@gmail.com>
2020-09-06 00:43:09 -0400

    Correçao de Logica incluindo lista

--- main.py
+++ main.py
@@ -7,25 +7,35 @@ pasta_atual = os.path.dirname(os.path.abspath(__file__))
 def cria_pasta():
     lista_arquivos, diretorios = indentificador_diretorio_Atual()
     for arquivo in lista_arquivos:
+
         arq = os.path.splitext(arquivo)[1][1:].capitalize()
-
-        if arquivo.lower() == 'main.py':
-            print(f"Arquivo {arquivo} ignorado")
-
-        else:
-            if arq not in diretorios and arq != "":
-
-                os.mkdir(pasta_atual + os.sep + arq)
-                print(f"Criando Pasta \033[32m{arq}\033[m")
-
-                shutil.move(arquivo, pasta_atual + os.sep + arq)
-                print(
-                    f"Movendo \033[33m{arquivo}\033[m Para \033[34m{arq}\033[m")
+        try:
+            if arquivo == __file__:
+                print(f"Arquivo {arquivo} ignorado")
 
             else:
-                shutil.move(arquivo, pasta_atual + os.sep + arq)
-                print(
-                    f"Movendo \033[33m{arquivo}\033[m Para \033[34m{arq}\033[m")
+                if arq not in diretorios and arq != "":
+
+                    diretorios.append(arq)
+                    os.mkdir(pasta_atual + os.sep + arq)
+                    print(f"Criando Pasta \033[32m{arq}\033[m")
+
+                    shutil.move(arquivo, pasta_atual + os.sep + arq)
+                    print(
+                        f"Movendo \033[33m{arquivo}\033[m Para \033[34m{arq}\033[m")
+
+                elif arq == "":
+
+                       print(f"Este Aquivos \033[31m{arquivo}\033[m nao Possui uma extensao Valida") 
+                       
+                else:
+
+                    shutil.move(arquivo, pasta_atual + os.sep + arq)
+                    print(
+                        f"Movendo \033[33m{arquivo}\033[m Para \033[34m{arq}\033[m")
+                
+        except FileExistsError:
+            print(f"Verificar este arquivo ==> {arquivo}")
 
             '''
             if arq not in diretorios and arq != "" and arquivo.lower() != "main.py":
@@ -43,7 +53,6 @@ def cria_pasta():
 
 
 def indentificador_diretorio_Atual():
-    lista_diretorios = []
     #lista_aquivos = os.listdir(pasta_atual)
     for (dirpath, dirnames, filenames) in os.walk(pasta_atual):
         if(dirpath == pasta_atual):
@@ -67,6 +76,7 @@ def main():
     else:
         print("\033[31mEscolha Invalida\033[m")
     print("\033[36mFim Do Programa\033[m")
+
 
 if __name__ == "__main__":
 
